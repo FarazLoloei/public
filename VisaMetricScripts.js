4dfd22daba432215241f9443c999d4220272a63c
FarazLoloei <FarazLoloei@users.noreply.github.com>
2023-07-07 03:33:31 +0330

    Add files via upload

--- VisaMetricScripts.js
+++ VisaMetricScripts.js
@@ -1,9 +1,9 @@
 document.cookie.replace(
     /(?<=^|;).+?(?=\=|;|$)/g, name => location.hostname
-    .split(/\.(?=[^\.]+\.)/)
-    .reduceRight((acc, val, i, arr) => i ? arr[i] = '.' + val + acc : (arr[i] = '', arr), '')
-    .map(domain => document.cookie = `${name}=;max-age=0;path=/;domain=${domain}`)
-  );
+        .split(/\.(?=[^\.]+\.)/)
+        .reduceRight((acc, val, i, arr) => i ? arr[i] = '.' + val + acc : (arr[i] = '', arr), '')
+        .map(domain => document.cookie = `${name}=;max-age=0;path=/;domain=${domain}`)
+);
 
 $("#result0").prop("checked", true);
 $("#result0").change();
@@ -17,33 +17,36 @@ $('#btnSubmit').click();
 var href = window.location.href;
 var correctPage = href.indexOf('appointment-form');
 
-if (correctPage  != -1){
+if (correctPage != -1) {
     console.info('start');
     var functionLoader = setInterval(() => {
         var fr = $.ajax({
             url: "https://ir-appointment.visametric.com/ir/appointment-form/getcity",
             method: 'POST',
             data: {
-              'applicationType': '3'
+                'applicationType': '3'
             },
             async: false,
-            success: function(response) {
-              $('#ajaxcity').html(response);
-              $(".city").prop("selectedIndex", 3);
-              $(".office").prop("selectedIndex", 1);
-              $(".officetype").prop("selectedIndex", 1);
-              $(".service_type").prop("selectedIndex", 0);
-              $(".totalPerson").prop("selectedIndex", 1);
+            success: function (response) {
+                $('#ajaxcity').html(response);
+                $(".city").prop("selectedIndex", 3);
+                $(".office").prop("selectedIndex", 1);
+                $(".officetype").prop("selectedIndex", 1);
+                $(".service_type").prop("selectedIndex", 0);
+                $(".totalPerson").prop("selectedIndex", 1);
             },
-            error: function() {}
-          });
-          if(fr.staus==200)
-          {clearInterval(functionLoader);}
+            error: function () { }
         });
-        $("#city").val(3);
-        $("#city").change();
+        if (fr.status == 200) { clearInterval(functionLoader); }
+    }, 500);
+
+    console.info('city loaded');
+    $("#city").val(3);
+    $("#city").change();
+    console.info('city selected');
+
     functionLoader = setInterval(() => {
-        var fr=$.ajax({
+        var fr = $.ajax({
             url: "https://ir-appointment.visametric.com/ir/appointment-form/getoffice",
             type: "POST",
             async: false,
@@ -57,14 +60,17 @@ if (correctPage  != -1){
                 $(".service_type").prop("selectedIndex", 0);
                 $(".totalPerson").prop("selectedIndex", 0);
             },
-            error: function (jqXHR, textStatus, errorThrown) {}
-        });
-        if(fr.staus==200)
-        {clearInterval(functionLoader);}
-        });
-        $("#office").val(1);
-        $("#office").change();
-    var functionLoader = setInterval(() => {
+            error: function (jqXHR, textStatus, errorThrown) { }
+        }, 500);
+        if (fr.status == 200) { clearInterval(functionLoader); }
+    });
+
+    console.info('office loaded');
+    $("#office").val(1);
+    $("#office").change();
+    console.info('office selected');
+
+    functionLoader = setInterval(() => {
         var fr = $.ajax({
             url: "https://ir-appointment.visametric.com/ir/appointment-form/getofficetype",
             type: "POST",
@@ -79,66 +85,66 @@ if (correctPage  != -1){
                 $(".officetype").prop("selectedIndex", 0);
                 $(".totalPerson").prop("selectedIndex", 0);
             },
-            error: function (jqXHR, textStatus, errorThrown) {}
+            error: function (jqXHR, textStatus, errorThrown) { }
         });
-        if(fr.staus==200)
-            {clearInterval(functionLoader);}
-    });
+        if (fr.status == 200) { clearInterval(functionLoader); }
+    }, 500);
+    console.info('office type loaded');
     $("#officetype").val(1);
-    $("#officetype").change();   
-    if($('#city option').length > 1 && $('#office option').length > 1 && $('#officetype option').length){
+    $("#officetype").change();
+    console.info('office type selected');
+
+    if ($('#city option').length > 1 && $('#office option').length > 1 && $('#officetype option').length) {
         $("#totalPerson").val(1);
         $("#totalPerson").change();
         if ($('.office').val() !== "0" && $('.officetype').val() !== "0" && $('.city').val() !== "0" && totalPerson > 0) {
             functionLoader = setInterval(() => {
-            var fr = $.ajax({
-                url: "https://ir-appointment.visametric.com/ir/appointment-form/getcalendarstatus",
-                type: "POST",
-                async: false,
-                data: {
-                    getvisaofficeid: 1,
-                    getservicetypeid: 1,
-                    getvisacountryid: '3'
-                },
-                success: function (response) {
-                    $('.setnewcalendarstatus').val(response);
-                    available_day_info_hide_control = response;
-                    if (response == 0) { $('#paytype').show(1000);}
-                },
-                error: function (jqXHR, textStatus, errorThrown) {}
-            });
-            if(fr.staus==200)
-            {clearInterval(functionLoader);}
-        });
+                var fr = $.ajax({
+                    url: "https://ir-appointment.visametric.com/ir/appointment-form/getcalendarstatus",
+                    type: "POST",
+                    async: false,
+                    data: {
+                        getvisaofficeid: 1,
+                        getservicetypeid: 1,
+                        getvisacountryid: '3'
+                    },
+                    success: function (response) {
+                        $('.setnewcalendarstatus').val(response);
+                        available_day_info_hide_control = response;
+                        if (response == 0) { $('#paytype').show(1000); }
+                    },
+                    error: function (jqXHR, textStatus, errorThrown) { }
+                });
+                if (fr.status == 200) { clearInterval(functionLoader); }
+            }, 500);
             var set_new_calendar_type = $('.setnewcalendarstatus').val();
             functionLoader = setInterval(() => {
-            var fr = $.ajax({
-                url: "https://ir-appointment.visametric.com/ir/appointment-form/getprice",
-                type: "POST",
-                async: false,
-                data: {
-                    serviceType: serviceType,
-                    totalPerson: totalPerson,
-                    getOfficeID: getOfficeID,
-                    calendarType: set_new_calendar_type,
-                    getConsular: '3'
-                },
-                success: function (response) {
-                    $('#totalFEE').html('جمع مبلغ: ' + response.data + ' ریال');
+                var fr = $.ajax({
+                    url: "https://ir-appointment.visametric.com/ir/appointment-form/getprice",
+                    type: "POST",
+                    async: false,
+                    data: {
+                        serviceType: 1,
+                        totalPerson: 1,
+                        getOfficeID: 1,
+                        calendarType: set_new_calendar_type,
+                        getConsular: '3'
+                    },
+                    success: function (response) {
+                        $('#totalFEE').html('جمع مبلغ: ' + response.data + ' ریال');
 
-                    $('.parentTotalFee').show(1000);
-                    $('#paytype').show(1000);
+                        $('.parentTotalFee').show(1000);
+                        $('#paytype').show(1000);
 
 
-                },
-                error: function (jqXHR, textStatus, errorThrown) {
-                    let err = $.parseJSON(jqXHR.responseText).firstAvailableDate;
-                    console.log(err);
-                }
-            });
-            if(fr.staus==200)
-            {clearInterval(functionLoader);}
-        });
+                    },
+                    error: function (jqXHR, textStatus, errorThrown) {
+                        let err = $.parseJSON(jqXHR.responseText).firstAvailableDate;
+                        console.log(err);
+                    }
+                });
+                if (fr.status == 200) { clearInterval(functionLoader); }
+            }, 500);
         } else {
             $('#availableDayInfo').hide();
             $('.parentTotalFee').hide();
